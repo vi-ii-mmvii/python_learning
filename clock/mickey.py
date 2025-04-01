dc411ae96594fe220e031957a8ae3fd618058909
vi-ii-mmvii <nur.adina.muratkyzy@gmail.com>
2025-04-01 13:40:56 +0500

    Update mickey.py

--- clock/mickey.py
+++ clock/mickey.py
@@ -4,15 +4,15 @@ import datetime
 pygame.init()
 
 
-SCREEN_WIDTH, SCREEN_HEIGHT = 1440, 1050
+SCREEN_WIDTH, SCREEN_HEIGHT = 1440, 1080
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Clock")
 clock = pygame.time.Clock()
 
 
-bg = pygame.image.load("clock/clock.jpg")
-second_hand = pygame.image.load("clock/sec.png")
-minute_hand = pygame.image.load("clock/min.png")
+bg = pygame.image.load("./clock/clock.jpg")
+second_hand = pygame.image.load("./clock/sec.png")
+minute_hand = pygame.image.load("./clock/min.png")
 
 
 def blit_rotate(image, pos, angle):
@@ -26,13 +26,14 @@ while running:
     clock.tick(60)
     
     for event in pygame.event.get():
-        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+        if event.type == pygame.QUIT or \
+            (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             running = False
 
     now = datetime.datetime.now()
 
-    second_angle = -now.second * 6 + 90
-    minute_angle = -now.minute * 6 + 90
+    second_angle = -now.second / 60 * 360 + 145
+    minute_angle = -now.minute / 60 * 360 - 30
 
     center_pos = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
 
